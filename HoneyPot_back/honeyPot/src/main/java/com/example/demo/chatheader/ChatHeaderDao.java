c6ff1edc8962c88684bb5d247c7119fabb1f4896
DESKTOP-LF71115\shon <sonsy0909@gmail.com>
2023-08-09 10:12:59 +0900

    chat 기능 dao  2023/08/09

---
+++ HoneyPot_back/honeyPot/src/main/java/com/example/demo/chatheader/ChatHeaderDao.java
@@ -0,0 +1,7 @@
+package com.example.demo.chatheader;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatHeaderDao extends JpaRepository<ChatHeader, Integer> {
+
+}
