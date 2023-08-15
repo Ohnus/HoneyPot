8c154cc7574f7d77af840bdd7fb753c420326276
DESKTOP-LF71115\shon <sonsy0909@gmail.com>
2023-08-16 08:43:13 +0900

    WebSoket develop 2023-08-16

--- HoneyPot_back/honeyPot/src/main/java/com/example/demo/WebSocketConfig.java
+++ HoneyPot_back/honeyPot/src/main/java/com/example/demo/WebSocketConfig.java
@@ -5,6 +5,8 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import com.example.demo.sockethandler.EchoHandler;
+
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer{
@@ -12,7 +14,11 @@ public class WebSocketConfig implements WebSocketConfigurer{
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		// TODO Auto-generated method stub
-		
+
+//			registry.addHandler(new ReplyEchoHandler(), "/replyEcho").setAllowedOrigins("*"); //pure websocket
+			
+			// sockJS 방식
+			registry.addHandler(new EchoHandler(), "/replyEcho").setAllowedOrigins("*").withSockJS();
 	}
 	
 
--- HoneyPot_back/honeyPot/src/main/java/com/example/demo/sockethandler/EchoHandler.java
+++ HoneyPot_back/honeyPot/src/main/java/com/example/demo/sockethandler/EchoHandler.java
@@ -1,10 +1,35 @@
 package com.example.demo.sockethandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class EchoHandler extends TextWebSocketHandler{
 
-
+List<WebSocketSession> sessions = new ArrayList<>();
 	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception{
+		System.out.println("afterConnectionEstablished:" + session);
+		sessions.add(session);
+	}
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		System.out.println("handleTextMessage:" + session + " : " + message);
+		String senderId = session.getId();
+		for (WebSocketSession sess: sessions) {
+			sess.sendMessage(new TextMessage(senderId + ": " + message.getPayload()));
+		}
+	}	
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println("afterConnectionEstablished:" + session + ":" + status);
+	}	
 
 }
